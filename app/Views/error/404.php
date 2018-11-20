85a2dd09075ae8b287c886eff25b5a946d60b014
Danilo <danilodesenvolvedorweb@hotmail.com>
2018-11-20 19:40:00 -0200

    Arquivos base criados

---
+++ app/Views/error/404.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>404. Página não encontrada!</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1 class="text-center mt-5 pt-5">404. Página não encontrada!</h1>
+    </body>
+</html>
--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation"></button>
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler d-lg-none border-white" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white"></i>
+    </button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             
